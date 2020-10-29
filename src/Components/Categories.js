7a00cbfddebe3fafb24ac4ba677eaa0e69d1d3c8
Mhurta41 <Mhurta41@gmail.com>
2020-10-29 18:59:28 -0500

    formatted categories page

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import './Categories.css';
+import { Link } from 'react-router-dom';
 
 const Categories = () => {
-	const categoryUrl =
-		'https://www.thecocktaildb.com/api/json/v1/1/list.php?a=list';
 	return (
-		<div>
-			<h1>Categories</h1>
-		</div>
+		<>
+			<h1 className='title'>Categories</h1>
+			<section className='container'>
+				<div className='card'>
+					<div className='card-image'>
+						<img src='https://i.imgur.com/7ykCky6.jpg' alt='alcoholic-drink' />
+					</div>
+					<h2 className='category-title'>Alcoholic</h2>
+				</div>
+				<div className='card'>
+					<div className='card-image'>
+						<img
+							src='https://i.imgur.com/lwnbwNb.jpg'
+							alt='non-alcoholic-drink'
+						/>
+					</div>
+					<h2 className='category-title'>Non-Alcoholic</h2>
+				</div>
+				<div className='card'>
+					<div className='card-image'>
+						<img src='https://i.imgur.com/xk103ud.jpg' alt='eggnog' />
+					</div>
+					<h2 className='category-title'>Optional Alcohol</h2>
+				</div>
+			</section>
+		</>
 	);
 };
 
